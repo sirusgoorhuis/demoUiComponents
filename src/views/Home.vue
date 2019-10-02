8fe675d908f1da1f54b2b9c787fa1adce122c92a
ivovanderzee <ivovanderzee@live.nl>
2019-10-02 10:27:46 +0200

    Implemented the design for the left menu column. Created a data component that makes a list on the left side of the app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  
+   <LeftColumn />
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import LeftColumn from '@/components/LeftColumn.vue';
 
 export default {
-  name: 'home',
+  name: 'LeftColumn',
   components: {
-    HelloWorld,
+    LeftColumn,
   },
 };
 </script>
